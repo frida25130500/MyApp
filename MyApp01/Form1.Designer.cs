44ae8bc8d4f57e0253a1694510c8d2d2c8e58cf3
frida25130500 <alu.25130500@correo.itlalaguna.edu.mx>
2026-09-09 10:24:57 -0600

    ojala funcione

--- MyApp01/Form1.Designer.cs
+++ MyApp01/Form1.Designer.cs
@@ -29,12 +29,81 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lblfecha = new System.Windows.Forms.Label();
+            this.lblreloj = new System.Windows.Forms.Label();
+            this.tmrreloj = new System.Windows.Forms.Timer(this.components);
+            this.btnencender = new System.Windows.Forms.Button();
+            this.lblejecucion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblfecha
+            // 
+            this.lblfecha.AutoSize = true;
+            this.lblfecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfecha.Location = new System.Drawing.Point(120, 117);
+            this.lblfecha.Name = "lblfecha";
+            this.lblfecha.Size = new System.Drawing.Size(63, 69);
+            this.lblfecha.TabIndex = 0;
+            this.lblfecha.Text = "0";
+            // 
+            // lblreloj
+            // 
+            this.lblreloj.AutoSize = true;
+            this.lblreloj.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblreloj.Location = new System.Drawing.Point(120, 276);
+            this.lblreloj.Name = "lblreloj";
+            this.lblreloj.Size = new System.Drawing.Size(63, 69);
+            this.lblreloj.TabIndex = 1;
+            this.lblreloj.Text = "0";
+            // 
+            // tmrreloj
+            // 
+            this.tmrreloj.Interval = 1000;
+            this.tmrreloj.Tick += new System.EventHandler(this.tmrreloj_Tick);
+            // 
+            // btnencender
+            // 
+            this.btnencender.Location = new System.Drawing.Point(583, 316);
+            this.btnencender.Name = "btnencender";
+            this.btnencender.Size = new System.Drawing.Size(75, 23);
+            this.btnencender.TabIndex = 2;
+            this.btnencender.Text = "Encender";
+            this.btnencender.UseVisualStyleBackColor = true;
+            this.btnencender.Click += new System.EventHandler(this.btnencender_Click);
+            // 
+            // lblejecucion
+            // 
+            this.lblejecucion.AutoSize = true;
+            this.lblejecucion.Location = new System.Drawing.Point(129, 393);
+            this.lblejecucion.Name = "lblejecucion";
+            this.lblejecucion.Size = new System.Drawing.Size(134, 16);
+            this.lblejecucion.TabIndex = 3;
+            this.lblejecucion.Text = "Tiempo de ejecución";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblejecucion);
+            this.Controls.Add(this.btnencender);
+            this.Controls.Add(this.lblreloj);
+            this.Controls.Add(this.lblfecha);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblfecha;
+        private System.Windows.Forms.Label lblreloj;
+        private System.Windows.Forms.Timer tmrreloj;
+        private System.Windows.Forms.Button btnencender;
+        private System.Windows.Forms.Label lblejecucion;
     }
 }
 
--- MyApp01/Form1.cs
+++ MyApp01/Form1.cs
@@ -12,9 +12,51 @@ namespace MyApp01
 {
     public partial class Form1 : Form
     {
+        int contador = 0;
+        bool bandera = false;
+
+        int minutos = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmrreloj_Tick(object sender, EventArgs e)
+        {
+        DateTime tiempo = DateTime.Now;
+            lblreloj.Text = tiempo.ToString("HH:mm:ss");
+            lblfecha.Text = tiempo.ToString("MM-dd-yyyy");
+            contador++;
+            if (contador == 60)
+            {
+                minutos++;
+                contador = 0;
+
+            }
+            lblejecucion.Text = "Tiempo de ejcución: " + minutos.ToString() + "minutos llevas en la ventana";
+        }
+
+        private void btnencender_Click(object sender, EventArgs e)
+        {
+            if (bandera == false)
+            {
+                bandera = true;
+                tmrreloj.Enabled = true;
+                btnencender.Text = "Apagar";
+
+
+            }
+            else
+            {
+                bandera = false;
+                tmrreloj.Enabled = false;
+                btnencender.Text = "Encender";
+            }
         }
     }
 }
